65bea56cc0a00fb73125819116585009e3495262
sarikamahadik <97827031+sarikamahadik@users.noreply.github.com>
2022-10-13 16:23:35 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    projectName = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.projectName
+
+
+class Client(models.Model):
+    client_id = models.IntegerField()
+    client_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100)
+    clientProject = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.client_name
+
